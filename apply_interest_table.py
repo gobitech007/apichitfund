656f7ce6793cc4d8fa122af4ff1c53ec6d162aa1
gss2669@ <gobinath.samuvel@ziply.com>
2025-09-24 11:06:49 +0530

    mysql end point

--- apply_interest_table.py
+++ apply_interest_table.py
@@ -7,7 +7,8 @@ def apply_sql_script():
     """
     # Database connection parameters
     db_params = {
-        'host': 'localhost',
+        'host': '127.0.0.1',
+        'port': 3306,
         'user': 'root',
         'password': 'admin',
         'database': 'mychitfund',
--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@localhost/mychitfund"
+SQLALCHEMY_DATABASE_URL = "mysql://root:admin@127.0.0.1:3306/mychitfund"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
